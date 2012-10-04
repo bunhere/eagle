90a9970b1773d412a95656528149fb92985db217
Ryuan Choi <ryuan.choi@samsung.com>
2012-10-05 01:22:59 +0900

    Added simple build/run script

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# eagle
+#                                                     
+# Copyright (C) 2012 Ryuan Choi
+#                                                     
+# License LGPL-3, see COPYING file at project folder.
+
+# simple build script.
+help() {
+    echo "Usage $0 [options]"
+
+    echo
+    echo "Options:"
+    echo "  -1                  : build with webkit1 (default is to build with webkit2)"
+    echo "  --help              : show this help message and exit"
+}
+
+options=""
+build_directory="WebKit2"
+
+while [ $# -gt 0 ]
+do
+    if [ "$1" = "-1" ]
+    then
+        options="$options -DUSE_WEBKIT=On"
+        build_directory="WebKit"
+    fi
+    if [ "$1" = "--help" ]
+    then
+        help
+        exit;
+    fi
+    shift
+done
+
+mkdir -p BuildTmp
+cd BuildTmp
+
+mkdir -p $build_directory
+cd $build_directory
+
+cmake ../.. $options
+make -j4
+
+cd ../..
---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# eagle
+#                                                     
+# Copyright (C) 2012 Ryuan Choi
+#                                                     
+# License LGPL-3, see COPYING file at project folder.
+
+# simple run script.
+help() {
+    echo "Usage $0 [options]"
+
+    echo
+    echo "Options:"
+    echo "  -1                  : run with webkit1 (default is to run with webkit2)"
+    echo "  --help              : show this help message and exit"
+}
+
+build_directory="WebKit2"
+
+while [ $# -gt 0 ]
+do
+    if [ "$1" = "-1" ]
+    then
+        build_directory="WebKit"
+    fi
+    if [ "$1" = "--help" ]
+    then
+        help
+        exit;
+    fi
+    shift
+done
+
+./BuildTmp/$build_directory/eagle
+
